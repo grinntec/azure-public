1fb842ca6fbd1b768c4ebe0be29fbb4be797d9dc
neil grinnall <40019507+neilgri@users.noreply.github.com>
2023-01-04 10:40:57 +0100

    Update main.tf

--- example-terraform-code/main.tf
+++ example-terraform-code/main.tf
@@ -14,13 +14,10 @@ resource "azurerm_storage_account" "this" {
   account_kind                      = "StorageV2"
   account_tier                      = "Standard"
   account_replication_type          = "LRS"
-  cross_tenant_replication_enabled  = true
   enable_https_traffic_only         = true
   min_tls_version                   = "TLS1_2"
   allow_nested_items_to_be_public   = true
   shared_access_key_enabled         = true
-  public_network_access_enabled     = true
-  default_to_oauth_authentication   = false
   is_hns_enabled                    = false
   nfsv3_enabled                     = false
   
